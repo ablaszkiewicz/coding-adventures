e0d2cadcea5c06c8d7dec4fcaf820c972f5da981
Aleksander Blaszkiewicz <kqmdjc8@gmail.com>
2024-05-26 15:14:18 +0200

    add hash router

--- src/main.tsx
+++ src/main.tsx
@@ -2,22 +2,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { Menu } from "./menu/Menu";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { QuadTree } from "./quad-tree/QuadTree";
 
-const router = createBrowserRouter(
-    [
-        {
-            path: "/",
-            element: <Menu />,
-        },
-        {
-            path: "/quad-tree",
-            element: <QuadTree />,
-        },
-    ],
-    { basename: "/coding-adventures" }
-);
+const router = createHashRouter([
+    {
+        path: "/",
+        element: <Menu />,
+    },
+    {
+        path: "/quad-tree",
+        element: <QuadTree />,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <ChakraProvider
